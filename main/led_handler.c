1ea559c1368f0038cf00e41ac23dfc79c6414fb3
KevinTadashiii <kevintadashiii@gmail.com>
2026-02-08 18:46:39 +0700

    refactor(led): split LED configuration and initialization logic

--- main/led_handler.c
+++ main/led_handler.c
@@ -1,22 +1,30 @@
+// FreeRTOS
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+// ESP-IDF drivers
 #include <driver/gpio.h>
+
+// ESP-IDF logging
 #include <esp_log.h>
-#include <esp_err.h>
 
 void turn_off_led(const char *TAG, gpio_num_t led_pin);
 
-void initialize_led(const char *TAG, gpio_num_t led_pin) {
-    gpio_config_t io_conf = {
-        .intr_type = GPIO_INTR_DISABLE,
-        .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = (1ULL << led_pin),
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .pull_up_en = GPIO_PULLUP_DISABLE
-    };
+void configure_led(const char *TAG, gpio_config_t *io_conf, gpio_num_t led_pin) {
+    io_conf->intr_type = GPIO_INTR_DISABLE;
+    io_conf->mode = GPIO_MODE_OUTPUT;
+    io_conf->pin_bit_mask = (1ULL << led_pin);
+    io_conf->pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf->pull_up_en = GPIO_PULLUP_DISABLE;
 
-    ESP_ERROR_CHECK(gpio_config(&io_conf));
+    ESP_LOGI(TAG, "LED configuration set for GPIO %d", led_pin);
+
+    return;
+}
+
+void initialize_led(const char *TAG, gpio_config_t *io_conf, gpio_num_t led_pin) {
+    ESP_ERROR_CHECK(gpio_config(io_conf));
+    
     // Ensure the LED is off at initialization
     turn_off_led(TAG, led_pin);
     ESP_LOGI(TAG, "LED initialized on GPIO %d", led_pin);
--- main/led_handler.h
+++ main/led_handler.h
@@ -1,6 +1,8 @@
 #include <driver/gpio.h>
 
-void initialize_led(const char *TAG, gpio_num_t led_pin);
+void configure_led(const char *TAG, gpio_config_t *io_conf, gpio_num_t led_pin);
+
+void initialize_led(const char *TAG, gpio_config_t *io_conf, gpio_num_t led_pin);
 
 void turn_on_led(const char *TAG, gpio_num_t led_pin);
 
