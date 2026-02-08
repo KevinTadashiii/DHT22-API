279a0a1d0252d5a5bda178cc909036b1f5452f02
KevinTadashiii <kevintadashiii@gmail.com>
2026-02-08 16:14:24 +0700

    refactor(http): update HTTP server configuration to accept TAG argument

--- main/http_manager.h
+++ main/http_manager.h
@@ -1,3 +1,3 @@
-void configure_http_server(httpd_config_t *config, httpd_handle_t *server);
+void configure_http_server(const char *TAG, httpd_config_t *config, httpd_handle_t *server);
 
 void configure_http_uri(const char *TAG, httpd_uri_t *dht_uri, esp_err_t (*dht_data_handler)(httpd_req_t *req));
